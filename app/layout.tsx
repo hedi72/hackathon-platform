8d27630c826c184c952d991815f2d8703bb3ec29
wiem jebari <wiem.jebari@esprit.tn>
2025-12-15 14:43:52 +0100

    font outfit impl

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,17 @@ import "./globals.css";
 import { UserProvider } from "@/app/context/UserContext";
 import { TokenProvider } from "@/app/context/TokenContext";
 import { Toaster } from "@/components/ui/toaster";
+import { Outfit } from "next/font/google";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  variable: "--font-outfit",
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={outfit.variable}>
       <body>
         <TokenProvider>
           <UserProvider>
